25a71143aef36515088f94a6aa788d7ad7a2f82a
Jakov1970 <45370614+Jakov1970@users.noreply.github.com>
2020-09-20 14:36:52 +0100

    Navbar completed all

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,7 +31,7 @@ function Navbar() {
         <div>
             <nav className='navbar'>
                 <div className='navbar-container'> 
-                    <Link to='/' className='navbar-logo'>
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
                         TRVL <MdCardTravel />
                     </Link>
                     <div className='menu-icon' onClick={handleClick}>
