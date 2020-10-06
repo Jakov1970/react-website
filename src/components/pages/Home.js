08f51399e0335369b7ebcfcd7aa56a38fc5b42b6
Jakov1970 <45370614+Jakov1970@users.noreply.github.com>
2020-10-06 15:23:15 +0100

    last change

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -4,14 +4,12 @@ import React from 'react'
 import Cards from '../Cards';
 import Footer from '../Footer';
 
-
-
 function Home() {
     return(
         <div>
             <HeroSection />
             <Cards />   
-            <Footer /> 
+            <Footer />
         </div>
       )
 }
